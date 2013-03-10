d9a020b42ff146faaef35773fd75ab3c83432433
Hack Reactor Students <accounts@hackreactor.com>
2013-03-09 17:03:50 -0800

    Complete Paybuddy HTML structure

--- validate.js
+++ validate.js
@@ -1,11 +1,13 @@
-$(function(){
-
-  var formHandler = function(event){
+$(document).ready(function() {
+  var formHandler = function(event) {
     console['log']("submitted"); // fixme: validate form here
-
     event['preventDefault'](); // prevents the page from reloading
   };
 
   $("form")['submit'](formHandler); // register a callback
 
+  $('#email_conf').click(function() {
+    $('.payment_conf').toggle();
+  });
 });
+
